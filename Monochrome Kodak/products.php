17b3a577f4808c2431a1190ef2dfe76af74cc23a
Rajike <68584258+Rajike@users.noreply.github.com>
2022-03-25 21:45:01 +0530

    Update products.php

--- Monochrome Kodak/products.php
+++ Monochrome Kodak/products.php
@@ -45,9 +45,9 @@
 <table id="footer" width="1000" align="center" padding="0">
 	<tr><td height="400"></td></tr>
 	<tr><td><a href="faq.html"><img src="images/faq.png" width="200" height="50"></td>
-		<td><a href="https://www.facebook.com/CrystalVisionCentre/"><img src="images/fb.png" width="200" height="50"></td>
-		<td><a href="https://twitter.com/crystalvisioncl"><img src="images/twi.png" width="200" height="50"></td>
-		<td><a href="https://www.google.com/maps/dir/7.427965,80.3555878/Crystal+Vision+eye/@7.1889919,79.8286303,10z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3ae258e4dacdc84d:0x3b3efd0ac37ae7bc!2m2!1d79.8624609!2d6.9481628"><img src="images/sto.png" width="200" height="50"></td>
+		<td><a href=""><img src="images/fb.png" width="200" height="50"></td>
+		<td><a href=""><img src="images/twi.png" width="200" height="50"></td>
+		<td><a href=""><img src="images/sto.png" width="200" height="50"></td>
 		<td><a href="toc.html"><img src="images/tnc.png" width="200" height="50"></td>
 	</tr>
 	</table>	
